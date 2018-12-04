1fa3e250f29e20f783cacdc010cd2400f1458062
Ubuntu <ubuntu@ip-172-31-21-184.us-east-2.compute.internal>
2018-12-04 13:20:44 -0800

    sparseData function created and prepSoup() finished

--- craigs/craigs.py
+++ craigs/craigs.py
@@ -4,6 +4,8 @@
 #
 #
 ##
+#from urllib.parse import urlparse
+import requests
 from bs4 import BeautifulSoup
 
 ##
@@ -13,16 +15,58 @@ from bs4 import BeautifulSoup
 #desc: asks user for URL link
 ##
 def getURL():
-    url=input("Enter URL: \n")
+    #url=input("Enter URL: \n")
+    url=input()
+    ## URL PARSER TBC
+    #urlVer=urlparse(url)
+    #print(urlVer)
+    ##
     return url
+    
+##
+#name: prepSoup
+#para: String url
+#retu: soup object
+#desc: creates soup object and returns the object
+##
+def prepSoup(url):
+    #get html page
+    #rawHTML = requests.get(url).text
+    #
+    localLink=open("/home/ubuntu/Enviornments/webscrape/proj/craigs/htmlfile.html")
+    ##
+    #soup = BeautifulSoup(rawHTML,"html.parser")
+    soup=BeautifulSoup(localLink.read(),'html.parser')
+    
+    #print(soup.prettify())
+    return soup
+
+##
+#name:
+#para:
+#retu:
+#desc:
+##
+def sparseData(soup):
+    divList = soup.find("div", class_="content").ul
+    ##for loop
+    for title in divList.find_all("p", class_="result-info"): #ul -> p -> a -> text
+        #a -> text
+        print(title.a.text)
+    return
+
 ##
 #name: main()
 #para: none
+#retu: 
 #desc: main function to execute code
 ##
 def main():
     url=getURL()
-    print(url)
+    #print(url)
+    soup=prepSoup(url)
+    sparseData(soup)
+    
 ##
 #name:
 #para:
